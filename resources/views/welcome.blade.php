73d7a03cfb89d1e41f4e81a37fd99a9e0338d140
wngur6076 <wngur6076@naver.com>
2022-03-09 01:45:58 +0900

    젠키스 테스트

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,6 @@
 @section('content')
     <section class="t-flex-grow t-flex t-items-center t-justify-center">
         <span>
-            젠키스 테스트!
             유니콘은 당신의 문제를 해결합니다.
         </span>
     </section>
